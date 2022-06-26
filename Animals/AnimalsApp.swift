0cd9c914777bd78a8f2bc33fd87216ab3832d8ae
G3N <fengyuan.yap@gmail.com>
2022-06-26 11:29:39 +0900

    Initial Commit

---
+++ Animals/AnimalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalsApp.swift
+//  Animals
+//
+//  Created by Feng Yuan Yap on 2022/06/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
