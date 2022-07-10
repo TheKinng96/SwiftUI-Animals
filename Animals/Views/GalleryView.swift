d18275996577dc7248acb6b0b95487fbf65e8b6a
G3N <fengyuan.yap@gmail.com>
2022-07-10 11:45:48 +0900

    created grid items for gallery

--- Animals/Views/GalleryView.swift
+++ Animals/Views/GalleryView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct GalleryView: View {
+  @State private var selectedAnimal: String = "lion"
+  let animals: [AnimalModel] = Bundle.main.decode("animals.json")
+  let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+  
   var body: some View {
-    ScrollView {
-      Text("Gallery View")
+    ScrollView(.vertical, showsIndicators: false) {
+      VStack(alignment: .center, spacing: 30) {
+        Image(selectedAnimal)
+          .resizable()
+          .scaledToFit()
+          .clipShape(Circle())
+          .overlay {
+            Circle().stroke(.white, lineWidth: 4)
+          }
+        
+        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+          ForEach(animals) { animal in
+            Image(animal.image)
+              .resizable()
+              .scaledToFit()
+              .clipShape(Circle())
+              .overlay {
+                Circle().stroke(.white, lineWidth: 1)
+              }
+              .onTapGesture {
+                selectedAnimal = animal.image
+              }
+          } //: LOOP
+        } //: VSTACK
+      } //:GRID
     } //: SCROLL
     .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
     .background(GalleryBackgroundView())
