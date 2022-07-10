531f6506bb1dfc0376de127b7924701ac3c9b582
G3N <fengyuan.yap@gmail.com>
2022-07-10 16:31:16 +0900

    finished up gallery view

--- Animals/Views/GalleryView.swift
+++ Animals/Views/GalleryView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct GalleryView: View {
   @State private var selectedAnimal: String = "lion"
+  @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+  @State private var gridColumn: Double = 3.0
   let animals: [AnimalModel] = Bundle.main.decode("animals.json")
-  let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+  let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
+  
+  func onGridChanged() {
+    gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColumn))
+  }
   
   var body: some View {
     ScrollView(.vertical, showsIndicators: false) {
@@ -21,6 +27,14 @@ struct GalleryView: View {
           .clipShape(Circle())
           .overlay {
             Circle().stroke(.white, lineWidth: 4)
+          }
+        
+        Slider(value: $gridColumn, in: 2...4, step: 1)
+          .padding()
+          .onChange(of: gridColumn) { value in
+            withAnimation(.easeIn) {
+              onGridChanged()
+            }
           }
         
         LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
@@ -34,10 +48,14 @@ struct GalleryView: View {
               }
               .onTapGesture {
                 selectedAnimal = animal.image
+                hapticImpact.impactOccurred()
               }
           } //: LOOP
         } //: VSTACK
       } //:GRID
+      .onAppear {
+        onGridChanged()
+      }
     } //: SCROLL
     .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
     .background(GalleryBackgroundView())
